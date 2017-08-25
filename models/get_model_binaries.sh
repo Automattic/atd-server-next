f7f649f90007307680780c1eec1948e3610ef46c
Michael Harzheim <michael.harzheim@gmail.com>
2017-08-25 21:27:21 +0200

    call bin/buildrules.sh to create rules.bin

--- models/get_model_binaries.sh
+++ models/get_model_binaries.sh
@@ -13,6 +13,6 @@ svn export https://openatd.svn.wordpress.org/atd-server/models/model.zip
 svn export https://openatd.svn.wordpress.org/atd-server/models/network3f.bin
 svn export https://openatd.svn.wordpress.org/atd-server/models/network3p.bin
 svn export https://openatd.svn.wordpress.org/atd-server/models/not_misspelled.txt
-svn export https://openatd.svn.wordpress.org/atd-server/models/rules.bin
 svn export https://openatd.svn.wordpress.org/atd-server/models/stringpool.bin
 svn export https://openatd.svn.wordpress.org/atd-server/models/trigrams.bin
+bin/buildrules.sh
