15b4a2529d8f232c865fb2592e5a079f278c18eb
Michael Harzheim <michael.harzheim@gmail.com>
2017-08-26 02:12:52 +0200

    uncomment export model.bin and model.zip

--- models/get_model_binaries.sh
+++ models/get_model_binaries.sh
@@ -8,8 +8,8 @@ svn export https://openatd.svn.wordpress.org/atd-server/models/hnetwork.bin ./mo
 svn export https://openatd.svn.wordpress.org/atd-server/models/hnetwork2.bin ./models/hnetwork2.bin
 svn export https://openatd.svn.wordpress.org/atd-server/models/hnetwork4.bin ./models/hnetwork4.bin
 svn export https://openatd.svn.wordpress.org/atd-server/models/lexicon.bin ./models/lexicon.bin
-#svn export https://openatd.svn.wordpress.org/atd-server/models/model.bin ./models/model.bin
-#svn export https://openatd.svn.wordpress.org/atd-server/models/model.zip ./models/model.zip
+svn export https://openatd.svn.wordpress.org/atd-server/models/model.bin ./models/model.bin
+svn export https://openatd.svn.wordpress.org/atd-server/models/model.zip ./models/model.zip
 svn export https://openatd.svn.wordpress.org/atd-server/models/network3f.bin ./models/network3f.bin
 svn export https://openatd.svn.wordpress.org/atd-server/models/network3p.bin ./models/network3p.bin
 svn export https://openatd.svn.wordpress.org/atd-server/models/not_misspelled.txt ./models/not_misspelled.txt
